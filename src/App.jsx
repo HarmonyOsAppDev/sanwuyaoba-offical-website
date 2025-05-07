e0291920649e34277f82f566ef8685f1f472241f
JeffWu <isjeffcom@gmail.com>
2025-05-07 14:47:42 +0800

    add privacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
-import OfficalHome from './OfficalHome.jsx'
+import OfficialHome from './OfficialHome.jsx'
 import { Routes, Route } from 'react-router-dom'
 import UserAgreement from './pages/UserAgreement'
+import PrivacyPolicy from './pages/PrivacyPolicy'
 
 function App() {
 
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<OfficalHome />} />
+        <Route path="/" element={<OfficialHome />} />
         <Route path="/user-agreement" element={<UserAgreement />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
     </div>
   )
