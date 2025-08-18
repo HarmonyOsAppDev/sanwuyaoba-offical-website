343fa170f7e132e0e54e312a150a85e51bb1493d
dubinbin <szdubinbin@gmail.com>
2025-08-18 19:16:21 +0800

    feat: 加备案

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,4 @@
-
+import baIcon from "../assets/ba.png"
 
 export default function Footer() {
   return (
@@ -7,7 +7,12 @@ export default function Footer() {
       <div className="flex flex-col md:flex-row justify-between items-center gap-4">
         <p className="text-sm text-gray-300">
           © 2025 SANWUYAOBA. All rights reserved. <span className="w-5"></span>
-          <a href="https://beian.miit.gov.cn/">粤ICP备2024349423号-1</a> 
+          <div className="flex items-center mt-1">
+            <img src={baIcon} alt="beian" className="w-4 h-4 mr-1" />
+            <a href="https://beian.mps.gov.cn/#/query/webSearch?code=44030002007729" rel="noreferrer" target="_blank">粤公网安备44030002007729号</a>
+            <span className="w-5"></span>
+            <a href="https://beian.miit.gov.cn/">粤ICP备2024349423号-1</a> 
+          </div>
         </p>
         <div className="flex gap-6">
           <a href="#" className="text-gray-300 hover:text-white transition-colors">
